87dbf1fde1a2ec2048eae91467da90e959c49217
Peter Kosyh <p.kosyh@gmail.com>
2018-07-05 14:42:03 +0300

    worn take

--- mp-ru.lua
+++ mp-ru.lua
@@ -221,7 +221,7 @@ mp.msg.Take.STATIC = "{#First} жестко {#word/закреплён,#first}."
 mp.msg.Take.SCENERY = "{#First/вн} невозможно взять."
 
 --"надет"
-mp.msg.Take.WORN = "{#First} {#word/надет,#first} на {#firstwhere/рд}."
+mp.msg.Take.WORN = "{#First} {#word/надет,#first} на {#firstwhere/пр,2}."
 mp.msg.Take.PARTOF = "{#First} является частью {#firstwhere/рд}."
 
 mp.msg.Remove.WHERE = "{#First} не находится {#if_has/#second,supporter,на,в} {#second/пр,2}."
