b93364fdb4f6934c0444119072e4a8fa106033f4
Peter Kosyh <p.kosyh@gmail.com>
2018-07-31 13:44:31 +0300

    up

--- demos/mars/main3.lua
+++ demos/mars/main3.lua
@@ -67,7 +67,7 @@ end)
 declare 'anim_titles' (function()
 	timer:set(50)
 	make_stars(stars_down)
-	D {'mars', 'img', 'gfx/logo.png',
+	D {'logo', 'img', 'gfx/logo.png',
 		x = 0,
 		y = 0,
 		z = 1,
@@ -175,6 +175,21 @@ declare 'pan_left' (function(v)
 		return
 	end
 	v.fx = v.fx + 1
+end)
+
+declare 'move_up' (function(v)
+	if _'titles'.finish then return end
+	v.y = v.y - 1
+	if v.y + v.h < 0 then
+		D {v.name} -- purge it
+	end
+end)
+
+declare 'move_down' (function(v)
+	if v.y >= theme.scr.h() / 2 then
+		return
+	end
+	v.y = v.y + 1
 end)
 
 declare 'pan_down' (function(v)
@@ -1734,15 +1749,87 @@ cutscene {
 	};
 	next_to = 'titles';
 }
+local titles = {
+	{"ДРУГОЙ МАРС", style = 1};
+	{ };
+	{"История и код:", style = 2};
+	{"Пётр Косых"},
+	{ };
+	{"По мотивам рассказов Р. Шекли:", style = 2},
+	{"Иной Марс"},
+	{"Мы одиноки"},
+	{ };
+	{"Иллюстрации:", style = 2},
+	{"Свободные изображения"},
+	{ };
+	{"Движок:", style = 2},
+	{"INSTEAD3"},
+	{"МЕТАПАРСЕР3"},
+	{"http://instead.syscall.ru", style = 2},
+	{ };
+	{"Альфа тестирование:"},
+	{"Сергей Можайский"},
+	{"Василий Воронков"},
+	{ };
+	{"КОНЕЦ", style = 1};
+}
 room {
 	nam = 'titles';
 	title = false;
 	dsc = false;
 	noparser = true;
-	enter = function()
+	{
+		finish = false;
+		offset = 0;
+		pos = 1;
+		line = titles[1];
+		ww = 0;
+		hh = 0;
+		font = false;
+		font_height = 0;
+		w = 0;
+		h = 0;
+	};
+	ini = function(s)
+		if here() == s then
+			s:enter()
+		end
+	end;
+	enter = function(s)
+		s.font_height = 16
+		s.w, s.h = std.tonum(theme.get 'scr.w'), std.tonum(theme.get 'scr.h')
 		D()
 		anim'titles'
 		fading.set {"fadeblack", max = FADE_LONG }
+	end;
+	timer = function(s)
+		local last ='text'..tostring(#titles)
+		if D(last) then
+			if not D'mars' then
+				D {'mars', 'img', 'gfx/mars2.png',
+				   x = theme.scr.w() / 2,
+				   xc = true;
+				   y = -256,
+				   yc = true;
+				   z = 2,
+				   process = move_down,
+				}
+			end
+			if D(last).y <= 8 then
+				s.finish = true
+			end
+			return false
+		end
+		s.offset = s.offset + 1
+		s.pos = math.floor(s.offset / s.font_height)
+		if s.pos > #titles or s.pos < 1 then
+			return false
+		end
+		if (D('text'..tostring(s.pos)) or not titles[s.pos][1]) then
+			return false
+		end
+		D{ 'text'..tostring(s.pos), "txt", titles[s.pos][1], w = theme.scr.w() - 260, x = 240, xc = false, y = theme.scr.h(), process = move_up, z = 1, style = titles[s.pos].style, size = 16 };
+		return false
 	end;
 }
 
