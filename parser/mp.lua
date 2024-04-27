d2834198f25b0dfd774893e44da4473a7bcab129
Peter Kosyh <pkosyh@yandex.ru>
2024-04-27 12:10:55 +0300

    2.4->2.5

--- parser/mp.lua
+++ parser/mp.lua
@@ -273,7 +273,7 @@ mp = std.obj {
 	inp_delim = " :.,!?-";
 	daemons = std.list {};
 	{
-		version = "2.4";
+		version = "2.5";
 		cache = { tokens = {}, nouns = false };
 		scope = std.list {};
 		logfile = false;
